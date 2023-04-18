84510fe15ef9c2e782c520bbd0d31cab6217b25c
Johana Pokorná <johanapokorna@Johana-MacBook-Air.local>
2023-04-18 21:34:16 +0200

    Alert function works

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import { findWinner } from 'https://unpkg.com/piskvorky@0.1.4';
 
-//ADD EVENT LISTENER TO BUTTONS
+//ADD CLASS FOR BUTTONS
 let currentPlayer = 'circle';
 
 const addClass = (event) => {
@@ -8,21 +8,20 @@ const addClass = (event) => {
     currentPlayer = 'cross';
     document.querySelector('#player').classList.remove('circle');
     document.querySelector('#player').classList.add('cross');
+    event.target.classList.add('board__field--circle');
     event.target.disabled = true;
-    return event.target.classList.add('board__field--circle');
-  }
-  if (currentPlayer === 'cross') {
+  } else if (currentPlayer === 'cross') {
     currentPlayer = 'circle';
     document.querySelector('#player').classList.remove('cross');
     document.querySelector('#player').classList.add('circle');
+    event.target.classList.add('board__field--cross');
     event.target.disabled = true;
-    return event.target.classList.add('board__field--cross');
   }
 
   //ARRAY
   const buttons = document.querySelectorAll('button');
-  // const buttonsArray = Array.from(buttons);
-  const herniPole = buttons.map((button) => {
+  const buttonsArray = Array.from(buttons);
+  const herniPole = buttonsArray.map((button) => {
     if (button.classList.contains('board__field--cross')) {
       return 'x';
     } else if (button.classList.contains('board__field--circle')) {
@@ -34,6 +33,7 @@ const addClass = (event) => {
 
   //WINNER
   const vitez = findWinner(herniPole);
+  console.log(vitez);
   if (vitez === 'o') {
     alert('Vyhrálo kolečko!');
   } else if (vitez === 'x') {
@@ -42,10 +42,13 @@ const addClass = (event) => {
     alert('Je to remíza!');
   }
 };
+
+//USE ,,addclass" FUNCTION FOR ALL BUTTONS
 document.querySelectorAll('button').forEach((oneButton) => {
   oneButton.addEventListener('click', addClass);
 });
 
+//RESTART
 const restart = (event) => {
   const prompt = confirm('Opravdu chcete začít znovu?');
 
